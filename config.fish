5f76ac944512d9cfddc959bd6ceb2345a730cb21
HaykalRadiandra <rthaykal@gmail.com>
2026-06-22 14:58:37 +0700

    update: config.fish

--- config.fish
+++ config.fish
@@ -3,6 +3,10 @@ set -gx ANDROID_HOME $HOME/Apps/Android/Sdk
 set -gx ANDROID_AVD_HOME $HOME/.config/.android/avd
 
 fish_add_path $HOME/.npm-global/bin/ $ANDROID_HOME/emulator $ANDROID_HOME/build-tools $ANDROID_HOME/platform-tools $ANDROID_HOME/cmdline-tools/latest/bin
+
+alias emulator="env QT_QPA_PLATFORM=xcb /home/haykalradiandra/Apps/Android/Sdk/emulator/emulator"
+alias genymotion="env QT_QPA_PLATFORM=xcb /usr/bin/genymotion"
+
 # ~/.config/fish/conf.d/00_init.fish
 fish_add_path $HOME/.cargo/bin/
 fish_add_path $HOME/.local/bin/
